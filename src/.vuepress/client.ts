5cdce3e6044a02e5afd892709d9a2f5b8789fd6d
DJ033016 <wujianbo@djbx.com>
2024-10-09 18:05:01 +0800

    背景模糊化

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -4,14 +4,14 @@ import { defineAsyncComponent } from 'vue';
 import 'vuepress-theme-hope/presets/bounce-icon.scss'; // 为页面图标添加鼠标悬停的跳动效果。
 
 const TopNavBeautify = defineAsyncComponent(() => import('./components/TopNavBeautify.vue'));
-// const HeroBG = defineAsyncComponent(() => import('./components/HeroBG.vue'));
+const HeroBG = defineAsyncComponent(() => import('./components/HeroBG.vue'));
 // const HeroHitokoto = defineAsyncComponent(() => import('./components/HeroHitokoto.vue'));
 // const NavMusic = defineAsyncComponent(() => import('./components/NavMusic.vue'));
 // const PrintVersion = defineAsyncComponent(() => import('./components/PrintVersion.vue'));
 // const CommentHideBtn = defineAsyncComponent(() => import('./components/CommentHideBtn.vue'));
 const MyIcon = defineAsyncComponent(() => import('./components/MyIcon.vue'));
-// const BlogBg = defineAsyncComponent(() => import('./components/BlogBg.vue'));
-// const BlogBeautify = defineAsyncComponent(() => import('./components/BlogBeautify.vue'));
+const BlogBg = defineAsyncComponent(() => import('./components/BlogBg.vue'));
+const BlogBeautify = defineAsyncComponent(() => import('./components/BlogBeautify.vue'));
 // const PreviewImage = defineAsyncComponent(() => import('./components/PreviewImage.vue'));
 
 export default defineClientConfig({
@@ -28,8 +28,8 @@ export default defineClientConfig({
     // NavMusic,
     // PrintVersion,
     // CommentHideBtn,
-    // BlogBeautify,
-    // BlogBg,
+    BlogBeautify,
+    BlogBg,
     // PreviewImage,
     // ...
   ],
